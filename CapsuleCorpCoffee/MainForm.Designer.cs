ca430ebb8430a66f920c3051470351e739071163
Luis Felipe <felipedelta0@gmail.com>
2020-01-09 23:54:09 -0300

    Primeiro commit. Em criação dos formulários. Padrão DAL escolhido, por enquanto.

---
+++ CapsuleCorpCoffee/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace CapsuleCorpCoffee
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConexao = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConexao
+            // 
+            this.btnConexao.Location = new System.Drawing.Point(264, 153);
+            this.btnConexao.Name = "btnConexao";
+            this.btnConexao.Size = new System.Drawing.Size(278, 115);
+            this.btnConexao.TabIndex = 0;
+            this.btnConexao.Text = "Testar Conexão";
+            this.btnConexao.UseVisualStyleBackColor = true;
+            this.btnConexao.Click += new System.EventHandler(this.btnConexao_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnConexao);
+            this.Name = "MainForm";
+            this.Text = "Capsule Corp Coffee";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConexao;
+    }
+}
+
---
+++ CapsuleCorpCoffee/MainForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Npgsql;
+
+namespace CapsuleCorpCoffee
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConexao_Click(object sender, EventArgs e)
+        {
+            var connString = "Host=localhost;Username=postgres;Password=postgres;Database=capsulecorpcoffeeDB";
+
+            try
+            {
+                var conn = new NpgsqlConnection(connString);
+
+                conn.Open();
+
+                MessageBox.Show(conn.State.ToString(), "Status da Conexão");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro");
+            }
+            
+        }
+    }
+}
